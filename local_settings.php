0c7fdbb0f361adf641fee8b4039c7ff424317625
nidal <nidal@level09.com>
2012-10-06 16:15:02 +0400

    drupal local configuraitonso

---
+++ local_settings.php
@@ -0,0 +1,18 @@
+<?php
+
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'localdb',
+      'username' => 'root',
+      'password' => '',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+  
+);
